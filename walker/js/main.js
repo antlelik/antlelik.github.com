1e5185f63e9925a9070c5a1a792dcf2862eb7b93
Alexey Antonov <antlelik@gmail.com>
2015-12-01 19:43:50 +0200

    change search clouds

--- walker/js/main.js
+++ walker/js/main.js
@@ -1,191 +1,180 @@
 (function() {
-	window.onload = function() {
-		walker.init();
-	};
+    window.onload = function() {
+        walker.init();
+    };
 
-	var walker = {
-		KEY_CODE_LEFT: 37,
-		KEY_CODE_TOP: 38,
-		KEY_CODE_RIGHT: 39,
-		KEY_CODE_BOTTOM: 40,
-		KEY_CODE_W: 87,
-		KEY_CODE_S: 83,
-		KEY_CODE_A: 65,
-		KEY_CODE_D: 68,
-		classTop: 'top',
-		classBottom: 'bottom',
-		classLeft: 'left',
-		classRight: 'right',
+    var walker = {
+        KEY_CODE_LEFT: 37,
+        KEY_CODE_TOP: 38,
+        KEY_CODE_RIGHT: 39,
+        KEY_CODE_BOTTOM: 40,
+        KEY_CODE_W: 87,
+        KEY_CODE_S: 83,
+        KEY_CODE_A: 65,
+        KEY_CODE_D: 68,
+        classTop: 'top',
+        classBottom: 'bottom',
+        classLeft: 'left',
+        classRight: 'right',
 
-		fieldUnit: {
-			size: {
-				width: 550,
-				height: 550
-			},
-			obstacle: [
-				[0,4,7,10],
-				[3,7,10],
-				[1,4],
-				[1,4,5,10],
-				[1,10],
-				[8],
-				[10],
-				[1,6,7,8,10],
-				[],
-				[0,4,7,10],
-				[1]
-			],
-			obstaclePositions: [],
-		},
-		fieldElement: null,
-		unitElement: null,
-		walkerUnit: {
-			step: null,
-			size: {
-				width: null,
-				height: null
-			},
-			position: {
-				left: null,
-				top: null
-			},
-			stepRatio: 5
-		},
-		init: function() {
-			this.setUnitInitialPosition();
-			this.checkKeyboardEvents();
-			this.drapMapObastacle();
-		},
-		checkKeyboardEvents: function() {
-			document.addEventListener('keydown', function(e) {
-				switch (e.keyCode) {
-					case this.KEY_CODE_TOP:
-					case this.KEY_CODE_W:
-						this.moveTop();
-						break;
-					case this.KEY_CODE_BOTTOM:
-					case this.KEY_CODE_S:
-						this.moveBottom();
-						break;
-					case this.KEY_CODE_LEFT:
-					case this.KEY_CODE_A:
-						this.moveLeft();
-						break;
-					case this.KEY_CODE_RIGHT:
-					case this.KEY_CODE_D:
-						this.moveRight();
-						break;
-				}
-				this.updateUnitPosition();
-			}.bind(this));
+        fieldUnit: {
+            size: {
+                width: 550,
+                height: 550
+            },
+            obstacle: [
+                [0, 4, 7, 10],
+                [3, 7, 10],
+                [1, 4],
+                [1, 4, 5, 10],
+                [1, 10],
+                [2],
+                [10],
+                [1, 6, 7, 8, 10],
+                [],
+                [0, 4, 7, 10],
+                [1]
+            ],
+            obstaclePositions: [],
+        },
+        fieldElement: null,
+        unitElement: null,
+        walkerUnit: {
+            step: null,
+            size: {
+                width: null,
+                height: null
+            },
+            obstacle: {
+                left: null,
+                top: null
+            },
+            stepRatio: 0.2
+        },
+        init: function() {
+            this.setUnitInitialPosition();
+            this.checkKeyboardEvents();
+            this.drapMapObastacle();
+        },
+        checkKeyboardEvents: function() {
+            document.addEventListener('keydown', function(e) {
+                switch (e.keyCode) {
+                    case this.KEY_CODE_TOP:
+                    case this.KEY_CODE_W:
+                        this.moveTop();
+                        break;
+                    case this.KEY_CODE_BOTTOM:
+                    case this.KEY_CODE_S:
+                        this.moveBottom();
+                        break;
+                    case this.KEY_CODE_LEFT:
+                    case this.KEY_CODE_A:
+                        this.moveLeft();
+                        break;
+                    case this.KEY_CODE_RIGHT:
+                    case this.KEY_CODE_D:
+                        this.moveRight();
+                        break;
+                }
+                this.updateUnitPosition();
+            }.bind(this));
 
-		},
-		moveTop: function() {
-			this.walkerUnit.position.top -= 1 * this.walkerUnit.step;
-			if (this.unitElement.offsetTop <= this.walkerUnit.step) {
-				this.walkerUnit.position.top = 0;
-			}
-			this.unitElement.className = this.classTop;
-			this.checkMoveTop(this.walkerUnit.position.left, this.walkerUnit.position.top);
-		},
-		moveBottom: function() {
-			this.walkerUnit.position.top += 1 * this.walkerUnit.step;
-			if (this.unitElement.offsetTop >= this.fieldUnit.size.height - this.walkerUnit.size.height) {
-				this.walkerUnit.position.top = this.fieldUnit.size.height - this.walkerUnit.size.height;
-			}
-			this.unitElement.className = this.classBottom;
-			this.checkMoveBottom(this.walkerUnit.position.left, this.walkerUnit.position.top);
-		},
-		moveLeft: function() {
-			this.walkerUnit.position.left -= 1 * this.walkerUnit.step;
-			if (this.unitElement.offsetLeft <= this.walkerUnit.step) {
-				this.walkerUnit.position.left = 0;
-			}
-			this.unitElement.className = this.classLeft;
-			this.checkMoveLeft(this.walkerUnit.position.left, this.walkerUnit.position.top);
-		},
-		moveRight: function() {
-			this.walkerUnit.position.left += 1 * this.walkerUnit.step;
-			if (this.unitElement.offsetLeft >= this.fieldUnit.size.width - this.walkerUnit.size.width) {
-				this.walkerUnit.position.left = this.fieldUnit.size.width - this.walkerUnit.size.width;
-			}
-			this.unitElement.className = this.classRight;
-			this.checkMoveRight(this.walkerUnit.position.left, this.walkerUnit.position.top);
-		},
-		setUnitInitialPosition: function() {
-			this.unitElement = document.querySelector('#unit');
-			this.fieldElement = document.querySelector('.map-section');
-			this.walkerUnit.size.width = this.unitElement.offsetWidth;
-			this.walkerUnit.size.height = this.unitElement.offsetHeight;
+        },
+        moveTop: function() {
+            var newObstacleTop = parseFloat((this.walkerUnit.obstacle.top - this.walkerUnit.stepRatio).toFixed(1));
+            var obstacleTop = Math.floor(newObstacleTop);
+            var leftPositionPrev = Math.floor(this.walkerUnit.obstacle.left);
+            var leftPositionNext = Math.ceil(this.walkerUnit.obstacle.left);
 
-			this.walkerUnit.step = this.walkerUnit.size.width / this.walkerUnit.stepRatio;
-			this.walkerUnit.position.left = (this.fieldElement.offsetWidth - this.walkerUnit.size.width) / 2;
-			this.walkerUnit.position.top = (this.fieldElement.offsetHeight - this.walkerUnit.size.height) / 2;
+            this.unitElement.className = this.classTop;
+            if (this.walkerUnit.obstacle.top === 0) {
+                return;
+            }
+            if (this.fieldUnit.obstacle[obstacleTop].indexOf(leftPositionPrev) > -1 || this.fieldUnit.obstacle[obstacleTop].indexOf(leftPositionNext) > -1) {
+                return;
+            }
 
-			this.updateUnitPosition();
-		},
-		updateUnitPosition: function() {
-			this.unitElement.style.left = this.walkerUnit.position.left + 'px';
-			this.unitElement.style.top = this.walkerUnit.position.top + 'px';
-		},
-		drapMapObastacle: function() {
-			var self = this,
-			    obstacleElement = document.createElement('div'),
-			    obstacleStr = '';
+            this.walkerUnit.obstacle.top = parseFloat((this.walkerUnit.obstacle.top - this.walkerUnit.stepRatio).toFixed(1));
+        },
+        moveBottom: function() {
+            var newObstacleTop = parseFloat((this.walkerUnit.obstacle.top + this.walkerUnit.stepRatio).toFixed(1));
+            var obstacleTop = Math.ceil(newObstacleTop);
+            var leftPositionPrev = Math.floor(this.walkerUnit.obstacle.left);
+            var leftPositionNext = Math.ceil(this.walkerUnit.obstacle.left);
+            
+            this.unitElement.className = this.classBottom;
+            if (this.walkerUnit.obstacle.top === (this.fieldUnit.size.height / this.walkerUnit.size.height) - 1) {
+                return;
+            }
+            if (this.fieldUnit.obstacle[obstacleTop] && (this.fieldUnit.obstacle[obstacleTop].indexOf(leftPositionPrev) > -1 || this.fieldUnit.obstacle[obstacleTop].indexOf(leftPositionNext) > -1)) {
+                return;
+            }
 
-			this.fieldUnit.obstacle.forEach(function(element, index){
-				element.forEach(function(el, ind){
-					obstacleStr += '<span class="obstacle" style="left: ' + self.walkerUnit.size.height * el + 'px; top: ' + self.walkerUnit.size.width * index + 'px;"></span>';
-					self.fieldUnit.obstaclePositions.push([self.walkerUnit.size.height * el, self.walkerUnit.size.width * index]);
-				});
-			});
+            this.walkerUnit.obstacle.top = parseFloat((this.walkerUnit.obstacle.top + this.walkerUnit.stepRatio).toFixed(1));
+        },
+        moveLeft: function() {
+            var newObstacleLeft = parseFloat((this.walkerUnit.obstacle.left - this.walkerUnit.stepRatio).toFixed(1));
+            var obstacleLeft = Math.floor(newObstacleLeft);
+            var topPositionPrev = Math.floor(this.walkerUnit.obstacle.top);
+            var topPositionNext = Math.ceil(this.walkerUnit.obstacle.top);
 
-			obstacleElement.innerHTML = obstacleStr;
-			obstacleElement.className = 'obstacle-list';
-			this.fieldElement.appendChild(obstacleElement);
+            this.unitElement.className = this.classLeft;
+            if (this.walkerUnit.obstacle.left === 0) {
+                return;
+            }
+            if (this.fieldUnit.obstacle[topPositionPrev].indexOf(obstacleLeft) > -1 || this.fieldUnit.obstacle[topPositionNext].indexOf(obstacleLeft) > -1) {
+                return;
+            }
 
-		},
+            this.walkerUnit.obstacle.left = parseFloat((this.walkerUnit.obstacle.left - this.walkerUnit.stepRatio).toFixed(1));
+        },
+        moveRight: function() {
+            var newObstacleLeft = parseFloat((this.walkerUnit.obstacle.left + this.walkerUnit.stepRatio).toFixed(1));
+            var obstacleLeft = Math.ceil(newObstacleLeft);
+            var topPositionPrev = Math.floor(this.walkerUnit.obstacle.top);
+            var topPositionNext = Math.ceil(this.walkerUnit.obstacle.top);
 
-		checkMoveTop: function(left, top) {
-			this.fieldUnit.obstaclePositions.forEach(function(element, index) {
-				// prevent move top through barrier
-				if ((top < element[1] + this.walkerUnit.size.height && top > element[1] - this.walkerUnit.size.height ) && (left > element[0] - this.walkerUnit.size.width && left < element[0] + this.walkerUnit.size.width ) ) {
-					this.walkerUnit.position.top = element[1] + this.walkerUnit.size.height;
-				}
-				this.updateUnitPosition();
-			}.bind(this));
-		},
+            this.unitElement.className = this.classRight;
+            if (this.walkerUnit.obstacle.left === (this.fieldUnit.size.width / this.walkerUnit.size.width) - 1) {
+                return;
+            }
+            if (this.fieldUnit.obstacle[topPositionPrev].indexOf(obstacleLeft) > -1 || this.fieldUnit.obstacle[topPositionNext].indexOf(obstacleLeft) > -1) {
+                return;
+            }
 
-		checkMoveBottom: function(left, top) {
-			this.fieldUnit.obstaclePositions.forEach(function(element, index) {
-				// prevent move bottom through barrier
-				if ((top > element[1] - this.walkerUnit.size.height && top < element[1] + this.walkerUnit.size.height ) && (left > element[0] - this.walkerUnit.size.width && left < element[0] + this.walkerUnit.size.width ) ) {
-					this.walkerUnit.position.top = element[1] - this.walkerUnit.size.height;
-				}
-				this.updateUnitPosition();
-			}.bind(this));
-		},
+            this.walkerUnit.obstacle.left = parseFloat((this.walkerUnit.obstacle.left + this.walkerUnit.stepRatio).toFixed(1));
+        },
+        setUnitInitialPosition: function() {
+            this.unitElement = document.querySelector('#unit');
+            this.fieldElement = document.querySelector('.map-section');
+            this.walkerUnit.size.width = this.unitElement.offsetWidth;
+            this.walkerUnit.size.height = this.unitElement.offsetHeight;
 
-		checkMoveLeft: function(left, top) {
-			this.fieldUnit.obstaclePositions.forEach(function(element, index) {
-				// prevent move left through barrier
-				if ((top > element[1] - this.walkerUnit.size.height && top < element[1] + this.walkerUnit.size.height ) && (left < element[0] + this.walkerUnit.size.width && left > element[0] ) ){
-					this.walkerUnit.position.left = element[0] + this.walkerUnit.size.width;
-				}
-				this.updateUnitPosition();
-			}.bind(this));
-		},
+            this.walkerUnit.step = this.walkerUnit.size.width / this.walkerUnit.stepRatio;
+            this.walkerUnit.obstacle.left = (this.fieldElement.offsetWidth - this.walkerUnit.size.width) / 2 / this.walkerUnit.size.width;
+            this.walkerUnit.obstacle.top = (this.fieldElement.offsetHeight - this.walkerUnit.size.height) / 2 / this.walkerUnit.size.height;
 
-		checkMoveRight: function(left, top) {
-			this.fieldUnit.obstaclePositions.forEach(function(element, index) {
-				// prevent move right through barrier
-				if ((top > element[1] - this.walkerUnit.size.height && top < element[1] + this.walkerUnit.size.height ) && (left > element[0] - this.walkerUnit.size.width && left < element[0] ) ){
-					this.walkerUnit.position.left = element[0] - this.walkerUnit.size.width;
-				}
-				this.updateUnitPosition();
-			}.bind(this));
-		}
-	};
+            this.updateUnitPosition();
+        },
+        updateUnitPosition: function() {
+            this.unitElement.style.left = this.walkerUnit.obstacle.left * this.walkerUnit.size.width + 'px';
+            this.unitElement.style.top = this.walkerUnit.obstacle.top * this.walkerUnit.size.height + 'px';
+        },
+        drapMapObastacle: function() {
+            var self = this,
+                obstacleElement = document.createElement('div'),
+                obstacleStr = '';
 
+            this.fieldUnit.obstacle.forEach(function(element, index) {
+                element.forEach(function(el, ind) {
+                    obstacleStr += '<span class="obstacle" style="left: ' + self.walkerUnit.size.height * el + 'px; top: ' + self.walkerUnit.size.width * index + 'px;"></span>';
+                    self.fieldUnit.obstaclePositions.push([self.walkerUnit.size.height * el, self.walkerUnit.size.width * index]);
+                });
+            });
 
+            obstacleElement.innerHTML = obstacleStr;
+            obstacleElement.className = 'obstacle-list';
+            this.fieldElement.appendChild(obstacleElement);
+        }
+    };
 })();
